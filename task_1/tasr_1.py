3dce0a30f0eb4823678738674e29dbc3c19f1586
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-27 22:07:58 +0300

    some changes (prepare tasks)

--- task_1/tasr_1.py
+++ task_1/tasr_1.py
@@ -5,3 +5,36 @@
 повторного використання вже обчислених значень чисел Фібоначчі.
 '''
 
+# ФУНКЦІЯ caching_fibonacci
+#     Створити порожній словник cache
+
+#     ФУНКЦІЯ fibonacci(n)
+#         Якщо n <= 0, повернути 0
+#         Якщо n == 1, повернути 1
+#         Якщо n у cache, повернути cache[n]
+
+#         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+#         Повернути cache[n]
+
+#     Повернути функцію fibonacci
+# КІНЕЦЬ ФУНКЦІЇ caching_fibonacci
+
+def caching_fibonacci(n):
+    cache = []
+
+    def fibonacci(n):
+        pass
+
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    
+    return fibonacci
+
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
--- task_2/task_2.py
+++ task_2/task_2.py
@@ -10,3 +10,23 @@
 цих чисел і обчислення загального прибутку.
 '''
 
+from typing import Callable
+
+text = "Загальний дохід працівника складається з декількох частин:" 
+"1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+def generator_numbers(text: str):
+    
+    yield
+
+
+def sum_profit(text: str, func: Callable):
+    numbers = []
+    
+      
+    return
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
+
+# Загальний дохід: 1351.46
