e47d2f6d3c55563c1f01add61f86cbae1ff96a1b
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-28 23:22:26 +0300

    task 1 done

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -5,26 +5,16 @@
 повторного використання вже обчислених значень чисел Фібоначчі.
 '''
 
-# ФУНКЦІЯ caching_fibonacci
-#     Створити порожній словник cache
-
-#     ФУНКЦІЯ fibonacci(n)
-#         Якщо n <= 0, повернути 0
-#         Якщо n == 1, повернути 1
-#         Якщо n у cache, повернути cache[n]
-
-#         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-#         Повернути cache[n]
-
-#     Повернути функцію fibonacci
-# КІНЕЦЬ ФУНКЦІЇ caching_fibonacci
-
-def caching_fibonacci(n):
-    cache = []
+def caching_fibonacci():
+    cache = {}
 
     def fibonacci(n):
-        pass
-
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
 
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
