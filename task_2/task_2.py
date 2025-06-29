bb10afcf9eb95e3bb2337fccd707073bd7348dec
andriy-h80 <andrey.hanzel@gmail.com>
2025-06-29 20:18:51 +0300

    task 2 done

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -12,19 +12,18 @@
 
 from typing import Callable
 
-text = "Загальний дохід працівника складається з декількох частин:" 
-"1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = ("Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.")
 
 def generator_numbers(text: str):
-    
-    yield
-
+    for item in text.split():
+        try:
+            number = float(item)
+            yield number
+        except:
+            continue
 
 def sum_profit(text: str, func: Callable):
-    numbers = []
-    
-      
-    return
+    return sum(func(text))
 
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
